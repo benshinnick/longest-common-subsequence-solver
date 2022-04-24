d83968597c82be900bcfe91137b678f9c78cca7b
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-24 17:29:17 -0500

    started outlining LcsSolver header file

--- src/lcs-solver.hpp
+++ src/lcs-solver.hpp
@@ -1,11 +1,25 @@
 #ifndef LCS_SOLVER_HPP
 #define LCS_SOLVER_HPP
 
+#include <string>
+
+#include "two-strings-retriever.hpp"
+
 class LcsSolver {
 
+    static const int MAX_SEQUENCE_LENGTH = 1000;
+    const std::string TWO_STRINGS_INPUT_FILE_NAME = "testTwoStrings.txt";
+
     private:
+        std::string firstSequence;
+        std::string secondSequence;
+        int costs[MAX_SEQUENCE_LENGTH + 1][MAX_SEQUENCE_LENGTH + 1];
+
+        std::string findLongestCommonSubsequence();
 
     public:
+        LcsSolver();
+        std::string getLongestCommonSubsequence();
 
 };
 
