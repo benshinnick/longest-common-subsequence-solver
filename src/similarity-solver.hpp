6f7b82fc27db15c9b94f16e7ff693a3ec416be95
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-25 19:28:11 -0500

    Wrote SimularitySolver class header file

--- src/similarity-solver.hpp
+++ src/similarity-solver.hpp
@@ -1,12 +1,33 @@
 #ifndef SIMILARITY_SOLVER_HPP
 #define SIMULARITY_SOLVER_HPP
 
+#include <sstream>
+
+#include "multi-strings-retriever.hpp"
+
 class SimilaritySolver {
 
     private:
+        int numStrings;
+        MultiStringsRetriever sequenceRetriever;
+
+        int calculateLcsLength(
+            const std::string& firstSequence,
+            const std::string& secondSequence
+        );
+        char calucateSimularityMeasure();
+
 
     public:
-    
+        SimilaritySolver();
+        SimilaritySolver(std::string multiStringsInputFileName);
+
+        char getSimularityMeasure(
+            const std::string& firstSequence,
+            const std::string& secondSequence
+        );
+        std::string getSimilarityTable();
+        void setSequenceRetriever(std::string multiStringsInputFileName);
 };
 
 #endif
