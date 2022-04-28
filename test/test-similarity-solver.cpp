0c775b699f35578939cbfc36641d41dc55598aec
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-28 14:50:02 -0500

    Tested similarity solver

--- test/test-similarity-solver.cpp
+++ test/test-similarity-solver.cpp
@@ -4,12 +4,13 @@
 
 #include <iostream>
 
-TEST_CASE("Testing Similarity Solver Constructors") {
+TEST_CASE("Testing Similarity Solver") {
+    SimilaritySolver similaritySolver = SimilaritySolver("testMultiStrings.txt");
+    REQUIRE(similaritySolver.getNumSequences() == 5);
+    std::cout << similaritySolver.getSimilarityTable() << std::endl;
 
-    SimilaritySolver similaritySolver1 = SimilaritySolver();
-    REQUIRE(similaritySolver1.getNumSequences() == 8);
-    SimilaritySolver similaritySolver2 = SimilaritySolver("simpleMultiStrings.txt");
-    REQUIRE(similaritySolver2.getNumSequences() == 26);
-
-    std::cout << similaritySolver1.getSimilarityTable() << std::endl;
+    REQUIRE(similaritySolver.getSimularityMeasure(0, 1) == 'H');
+    REQUIRE(similaritySolver.getSimularityMeasure(2, 3) == 'M');
+    REQUIRE(similaritySolver.getSimularityMeasure(2, 4) == 'L');
+    REQUIRE(similaritySolver.getSimularityMeasure(0, 2) == 'D');
 }
