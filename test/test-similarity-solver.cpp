3ddbd5ec075ca3b621fdb7c579e63ae4a3622d72
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-28 15:36:58 -0500

    Tested similarity solver lcs length computation

--- test/test-similarity-solver.cpp
+++ test/test-similarity-solver.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-TEST_CASE("Testing Similarity Solver") {
+TEST_CASE("Testing Similarity Solver Similarity Measures") {
     SimilaritySolver similaritySolver = SimilaritySolver("testMultiStrings.txt");
     REQUIRE(similaritySolver.getNumSequences() == 5);
     std::cout << similaritySolver.getSimilarityTable() << std::endl;
@@ -14,3 +14,14 @@ TEST_CASE("Testing Similarity Solver") {
     REQUIRE(similaritySolver.getSimularityMeasure(2, 4) == 'L');
     REQUIRE(similaritySolver.getSimularityMeasure(0, 2) == 'D');
 }
+
+TEST_CASE("Testing Similarity Solver LCS Length") {
+    SimilaritySolver similaritySolver = SimilaritySolver("testMultiStrings.txt");
+    REQUIRE(similaritySolver.getNumSequences() == 5);
+    
+    REQUIRE(similaritySolver.getLcsLength(0, 1) == 9);
+    REQUIRE(similaritySolver.getLcsLength(2, 3) == 16);
+    REQUIRE(similaritySolver.getLcsLength(2, 4) == 12);
+    REQUIRE(similaritySolver.getLcsLength(0, 2) == 9);
+}
+
